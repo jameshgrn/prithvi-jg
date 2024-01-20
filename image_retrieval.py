fe066357807a81ea6185659d857a7692f5b07d98
Jake Gearon <jake.gearon@gmail.com>
2024-01-20 13:51:52 -0500

    Add rasterio and affine imports for image retrieval

--- image_retrieval.py
+++ image_retrieval.py
@@ -1,6 +1,8 @@
 from pystac_client import Client
 from odc.stac import load
 import rasterio
+from rasterio.crs import CRS
+from affine import Affine
 
 client = Client.open("https://earth-search.aws.element84.com/v1")
 collection = "sentinel-2-l2a"
@@ -12,15 +14,19 @@ output_files = ["/N/u/jhgearon/Quartz/prithvi-jg/images/t1.tif", "/N/u/jhgearon/
 for i, time_interval in enumerate(time_intervals):
     search = client.search(collections=[collection], bbox=tas_bbox, datetime=time_interval)
     data = load(search.items(), bbox=tas_bbox, groupby="solar_day", chunks={})
-    crs = data['spatial_ref'].values
+    geotransform = tuple(map(float, data['spatial_ref'].attrs['GeoTransform'].split()))
+    affine_transform = Affine(*geotransform[:6])
+    crs_wkt = data['spatial_ref'].attrs['crs_wkt']
+    crs = rasterio.crs.CRS.from_wkt(crs_wkt)
     bands_to_save = ['blue', 'green', 'red', 'nir', 'swir16', 'swir22']
     band_count = len(bands_to_save)
     # Save the specified bands of the image to a tif file
     with rasterio.open(output_files[i], 'w', driver='GTiff', 
-                       height=data['red'].shape[1], 
-                       width=data['red'].shape[2], 
-                       count=band_count, 
-                       crs=crs,
-                       dtype=data['red'].dtype) as dst:
+                    height=data['red'].shape[1], 
+                    width=data['red'].shape[2], 
+                    count=band_count, 
+                    dtype=data['red'].dtype,
+                    crs=crs,
+                    transform=affine_transform) as dst:
         for j, band in enumerate(bands_to_save):
             dst.write(data[band].isel(time=0).values, j + 1)
