ab475f530d99113763a1d91718b85f3403ec1270
Jake Gearon <jake.gearon@gmail.com>
2024-02-08 00:03:47 -0500

    Update job name and output file names in runner.sh script

--- runner.sh
+++ runner.sh
@@ -9,9 +9,9 @@
 #SBATCH --time=0-2:59:00
 #SBATCH --mem=128gb
 #SBATCH --mail-type=FAIL,BEGIN,END
-#SBATCH --job-name=levity
-#SBATCH --output=lev.out
-#SBATCH --error=lev.err
+#SBATCH --job-name=prithvi
+#SBATCH --output=prithvi.out
+#SBATCH --error=prithvi.err
 
 module load python/gpu/3.10.10
 
@@ -24,6 +24,10 @@ t1="${img_path}t1.tif"
 t2="${img_path}t2.tif"
 t3="${img_path}t3.tif"
 
-python image_retrieval.py
+# Check if images exist
+if [[ ! -f "$t1" || ! -f "$t2" || ! -f "$t3" ]]; then
+    python image_retrieval.py
+fi
+
 python Prithvi_run_inference.py --data_files $t1 $t2 $t3 --yaml_file_path Prithvi_100M_config.yaml --checkpoint Prithvi_100M.pt --output_dir output --mask_ratio 0.5
 python image_plotting.py
